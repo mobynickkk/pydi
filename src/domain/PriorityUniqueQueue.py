46266b5a37a3359f55b464947ec1ff3493320fc2
mobynick <mobynick365@gmail.com>
2021-11-06 21:11:29 +0300

    Transfering PriorityUniqueQueue to Queue driven structure

--- src/domain/PriorityUniqueQueue.py
+++ src/domain/PriorityUniqueQueue.py
@@ -1,25 +1,34 @@
 import typing as T
-
-from .types import Class
+from queue import PriorityQueue
 
 
 class PriorityUniqueQueue:
-    values: T.List[T.Any]
-    queue: T.List[T.Tuple[Class, T.Any]]
+    __dict: dict
+    __queue: PriorityQueue
 
     def __init__(self):
-        self.values = list()
-        self.queue = list()
+        self.__dict = dict()
+        self.__queue = PriorityQueue()
 
     def put(self, value: T.Tuple[int, T.Any]):
-        if value[1] in self.values:
-            return
-        self.values.append(value[1])
-        self.queue.append(value)
-        self.queue.sort(key=lambda x: x[0])
+        self.__dict[value[1]] = min(self.__dict.get(value[1], self.Inf()), value[0])
+        self.__queue = PriorityQueue()
+        self.__put_all(self.__dict.items())
+        
+    def __put_all(self, values: T.Iterable[T.Tuple[int, T.Any]]):
+        for value in values:
+            self.__queue.put((value[1], value[0]))
 
-    def empty(self):
-        return len(self.queue) == 0
+    def empty(self) -> bool:
+        return self.__queue.empty()
 
-    def get(self):
-        return self.queue.pop(0)
+    def get(self) -> T.Any:
+        return self.__queue.get()[0]
+
+    class Inf:
+
+        def __gt__(self, other):
+            return True
+
+        def __lt__(self, other):
+            return False
